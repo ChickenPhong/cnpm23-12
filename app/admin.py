1f0d9caaeff17c8ba4e69bd791e680f269b89b7c
QBinh2102 <toquocbinh2102@gmail.com>
2024-12-26 19:01:10 +0700

    Cap nhat chuc nang admin

--- app/admin.py
+++ app/admin.py
@@ -16,6 +16,38 @@ from app.models import NhanVien, HocSinh, DanhSachLop, GiaoVienDayHoc, MonHoc, G
 
 admin = Admin(app=app, name='Người Quản Trị', template_mode='bootstrap4')
 
+class QuiDinhForm(FlaskForm):
+    min_age = IntegerField('Tuổi thấp nhất', validators=[validators.NumberRange(min=1)])
+    max_age = IntegerField('Tuổi cao nhất', validators=[validators.NumberRange(min=1)])
+    si_so = IntegerField('Sĩ số', validators=[validators.NumberRange(min=1)])
+    so_cot_15p = IntegerField('Số cột 15p', validators=[validators.NumberRange(min=1)])
+    so_cot_1tiet = IntegerField('Số cột 1 tiết', validators=[validators.NumberRange(min=1)])
+    so_cot_thi = IntegerField('Số cột thi', validators=[validators.NumberRange(min=1)])
+
+class QuiDinhView(ModelView):
+    form_columns = ['min_age', 'max_age', 'si_so', 'so_cot_15p', 'so_cot_1tiet', 'so_cot_thi']
+    column_list = ['min_age', 'max_age', 'si_so', 'so_cot_15p', 'so_cot_1tiet', 'so_cot_thi']
+    column_labels = {
+        'min_age': 'Tuổi thấp nhất',
+        'max_age': 'Tuổi cao nhất',
+        'si_so': 'Sĩ số',
+        'so_cot_15p': 'Số cột 15p',
+        'so_cot_1tiet': 'Số cột 1 tiết',
+        'so_cot_thi': 'Số cột thi'
+    }
+    can_create = False
+    can_delete = False
+    form=QuiDinhForm
+
+class PhongHocForm(FlaskForm):
+    tenPhong = IntegerField('Số phòng', validators=[validators.NumberRange(min=1)])
+
+class PhongHocView(ModelView):
+    column_list = ['tenPhong']
+    column_labels = {'tenPhong':'Số phòng'}
+    form = PhongHocForm
+    column_searchable_list = ['tenPhong']
+
 class MonHocForm(FlaskForm):
     tenMonHoc = StringField('Môn')
     soCot15p = IntegerField('Số cột 15p')
@@ -39,14 +71,94 @@ class MonHocView(ModelView):
         'soCotThi':'Số cột thi'
     }
     form = MonHocForm
+    column_searchable_list = ['tenMonHoc']
 
-class PhongHocForm(FlaskForm):
-    tenPhong = IntegerField('Số phòng', validators=[validators.NumberRange(min=1)])
+class NhanVienView(ModelView):
+    column_labels = {
+        'hoTen': 'Họ tên',
+        'gioiTinh': 'Giới tính',
+        'ngaySinh': 'Ngày sinh',
+        'diaChi': 'Địa chỉ',
+        'SDT': 'Số điện thoại',
+        'eMail': 'Email',
+        'taiKhoan': 'Tài khoản',
+        'matKhau': 'Mật khẩu',
+        'vaiTro': 'Vai trò'
+    }
+    column_searchable_list = ['hoTen']
+    column_filters = ['vaiTro','gioiTinh']
 
-class PhongHocView(ModelView):
-    column_list = ['tenPhong']
-    column_labels = {'tenPhong':'Số phòng'}
-    form = PhongHocForm
+    def on_model_change(self, form, model, is_created):
+        if form.matKhau.data:
+            model.matKhau = hashlib.md5(form.matKhau.data.encode('utf-8')).hexdigest()
+        super(NhanVienView, self).on_model_change(form, model, is_created)
+
+class GiaoVienView(ModelView):
+    column_list = ['hoTen', 'gioiTinh', 'ngaySinh', 'diaChi', 'SDT', 'eMail', 'monHoc', 'taiKhoan', 'matKhau']
+    form_columns = ['hoTen', 'gioiTinh', 'ngaySinh', 'diaChi', 'SDT', 'eMail', 'taiKhoan', 'matKhau', 'monHoc']
+    column_labels = {
+        'hoTen': 'Họ tên',
+        'gioiTinh': 'Giới tính',
+        'ngaySinh': 'Ngày sinh',
+        'diaChi': 'Địa chỉ',
+        'SDT': 'Số điện thoại',
+        'eMail': 'Email',
+        'monHoc': 'Môn',
+        'taiKhoan': 'Tài khoản ',
+        'matKhau': 'Mật khẩu',
+        'monHoc.tenMonHoc':'Môn học'
+    }
+    column_searchable_list = ['hoTen']
+    column_filters = ['monHoc.tenMonHoc','gioiTinh']
+
+    def on_model_change(self, form, model, is_created):
+        if form.matKhau.data:
+            model.matKhau = hashlib.md5(form.matKhau.data.encode('utf-8')).hexdigest()
+        super(GiaoVienView, self).on_model_change(form, model, is_created)
+
+class GiaoVienDayHocView(ModelView):
+    column_list = ['hoc_ky.namHoc', 'giaoVien', 'giaoVien.monHoc', 'lopDay']
+    column_labels = {
+        'hoc_ky.namHoc': 'Năm học',
+        'giaoVien': 'Giáo Viên',
+        'lopDay': 'Lớp',
+        'giaoVien.monHoc': 'Môn Học',
+        'lopDay.tenLop':'Lớp',
+        'giaoVien.monHoc.tenMonHoc':'Môn',
+        'giaoVien.hoTen':'Họ Tên'
+    }
+    can_create = False
+    can_edit = False
+    column_searchable_list = ['giaoVien.hoTen']
+    column_filters = ['hoc_ky.namHoc', 'lopDay.tenLop','giaoVien.monHoc.tenMonHoc']
+
+    def get_query(self):
+        # Thực hiện sắp xếp năm học giảm dần
+        return (
+            self.session.query(self.model)
+            .join(HocKy, HocKy.idHocKy == self.model.idHocKy)  # JOIN với bảng HocKy
+            .order_by(HocKy.namHoc.desc())  # Sắp xếp theo năm học giảm dần
+        )
+
+class HocSinhView(ModelView):
+    form_columns = ['hocSinhLop', 'hoTen', 'gioiTinh','ngaySinh', 'khoi','diaChi','SDT','eMail']
+    column_list = ['hoTen', 'gioiTinh', 'ngaySinh', 'khoi','diaChi','SDT','eMail','hocSinhLop']
+    column_labels = {
+        'hoTen': 'Họ tên',
+        'gioiTinh': 'Giới tính',
+        'ngaySinh': 'Ngày sinh',
+        'khoi':'Khối',
+        'diaChi':'Địa chỉ',
+        'SDT':'Số điện thoại',
+        'hocSinhLop':'Học lớp',
+        'hocSinhLop.tenLop':'Lớp'
+    }
+    can_create = False
+    column_searchable_list = ['hoTen']
+    column_filters = ['khoi', 'hocSinhLop.tenLop', 'gioiTinh']
+
+    def get_query(self):
+        return self.session.query(self.model).order_by(self.model.khoi.desc())
 
 class DanhSachLopView(ModelView):
     form_columns = ['tenLop', 'hocKy', 'giaoVienChuNhiem', 'phongHoc', 'siSo', 'active']
@@ -58,14 +170,12 @@ class DanhSachLopView(ModelView):
         'phongHoc':'Phòng',
         'siSoHienTai':"Sĩ số lớp",
         'siSo':'Sĩ số tối đa',
-        'active':'Trạng thái'
+        'active':'Trạng thái',
+        'giaoVienChuNhiem.hoTen':'Giáo Viên Chủ Nhiệm'
     }
     can_create = False
-
-    def can_edit(self, obj):
-        # Kiểm tra nếu active là False thì không cho sửa
-        return obj.active
-
+    column_searchable_list = ['giaoVienChuNhiem.hoTen']
+    column_filters = ['tenLop', 'hocKy.namHoc']
 
     def edit_form(self, obj=None):
         form = super(DanhSachLopView, self).edit_form(obj)
@@ -79,115 +189,9 @@ class DanhSachLopView(ModelView):
     def get_query(self):
         return self.session.query(self.model).order_by(self.model.active.desc(), self.model.tenLop)
 
-class GiaoVienView(ModelView):
-    column_list = ['hoTen', 'gioiTinh', 'ngaySinh', 'diaChi', 'SDT', 'eMail', 'monHoc', 'taiKhoan', 'matKhau']
-    form_columns = ['hoTen', 'gioiTinh', 'ngaySinh', 'diaChi', 'SDT', 'eMail', 'taiKhoan', 'matKhau', 'monHoc']
-    column_labels = {
-        'hoTen': 'Họ tên',
-        'gioiTinh': 'Giới tính',
-        'ngaySinh': 'Ngày sinh',
-        'diaChi': 'Địa chỉ',
-        'SDT': 'Số điện thoại',
-        'eMail': 'Email',
-        'monHoc': 'Môn học',
-        'taiKhoan': 'Tài khoản ',
-        'matKhau': 'Mật khẩu'
-    }
-
-    def on_model_change(self, form, model, is_created):
-        if form.matKhau.data:
-            model.matKhau = hashlib.md5(form.matKhau.data.encode('utf-8')).hexdigest()
-
-        super(GiaoVienView, self).on_model_change(form, model, is_created)
-
-class NhanVienView(ModelView):
-    #form_columns = ['hoTen', 'gioiTinh', 'ngaySinh', 'diaChi', 'SDT', 'eMail', 'taiKhoan', 'matKhau', 'vaiTro']
-
-    column_labels = {
-        'hoTen': 'Họ tên',
-        'gioiTinh': 'Giới tính',
-        'ngaySinh': 'Ngày sinh',
-        'diaChi': 'Địa chỉ',
-        'SDT': 'Số điện thoại',
-        'eMail': 'Email',
-        'taiKhoan': 'Tài khoản',
-        'matKhau': 'Mật khẩu',
-        'vaiTro': 'Vai trò'
-    }
-
-    def on_model_change(self, form, model, is_created):
-        if form.matKhau.data:
-            model.matKhau = hashlib.md5(form.matKhau.data.encode('utf-8')).hexdigest()
-
-        super(NhanVienView, self).on_model_change(form, model, is_created)
-
-class GiaoVienDayHocView(ModelView):
-    column_list = ['hoc_ky.namHoc', 'giaoVien', 'giaoVien.monHoc', 'lopDay']
-    column_labels = {
-        'hoc_ky.namHoc': 'Năm học',
-        'giaoVien': 'Giáo Viên',
-        'lopDay': 'Lớp',
-        'giaoVien.monHoc': 'Môn Học'
-    }
-
-    can_create = False
-    can_edit = False
-
-    def get_query(self):
-        # Thực hiện sắp xếp năm học giảm dần
-        return (
-            self.session.query(self.model)
-            .join(HocKy, HocKy.idHocKy == self.model.idHocKy)  # JOIN với bảng HocKy
-            .order_by(HocKy.namHoc.desc())  # Sắp xếp theo năm học giảm dần
-        )
-
-
-class HocSinhView(ModelView):
-    form_columns = ['hocSinhLop', 'hoTen', 'gioiTinh','ngaySinh', 'khoi','diaChi','SDT','eMail']
-    column_list = ['hoTen', 'gioiTinh', 'ngaySinh', 'khoi','diaChi','SDT','eMail','hocSinhLop']
-    column_labels = {
-        'hoTen': 'Họ tên',
-        'gioiTinh': 'Giới tính',
-        'ngaySinh': 'Ngày sinh',
-        'khoi':'Khối',
-        'diaChi':'Địa chỉ',
-        'SDT':'Số điện thoại',
-        'hocSinhLop':'Học lớp'
-    }
-    can_create = False  # Không cho phép thêm
-
-    def get_query(self):
-        # Sắp xếp active = True trước, sau đó theo tên lớp
-        return self.session.query(self.model).order_by(self.model.khoi.desc())
-
-class QuiDinhForm(FlaskForm):
-    min_age = IntegerField('Tuổi thấp nhất', validators=[validators.NumberRange(min=1)])
-    max_age = IntegerField('Tuổi cao nhất', validators=[validators.NumberRange(min=1)])
-    si_so = IntegerField('Sĩ số', validators=[validators.NumberRange(min=1)])
-    so_cot_15p = IntegerField('Số cột 15p', validators=[validators.NumberRange(min=1)])
-    so_cot_1tiet = IntegerField('Số cột 1 tiết', validators=[validators.NumberRange(min=1)])
-    so_cot_thi = IntegerField('Số cột thi', validators=[validators.NumberRange(min=1)])
-
-class QuiDinhView(ModelView):
-    column_labels = {
-        'min_age': 'Tuổi thấp nhất',
-        'max_age': 'Tuổi cao nhất',
-        'si_so': 'Sĩ số',
-        'so_cot_15p': 'Số cột 15p',
-        'so_cot_1tiet': 'Số cột 1 tiết',
-        'so_cot_thi': 'Số cột thi'
-    }
-    can_create = False
-    can_delete = False
-
-    form=QuiDinhForm
-
-    form_columns = ['min_age', 'max_age', 'si_so', 'so_cot_15p', 'so_cot_1tiet','so_cot_thi']
-    column_list = ['min_age', 'max_age', 'si_so', 'so_cot_15p', 'so_cot_1tiet','so_cot_thi']
-
 admin.add_view(QuiDinhView(QuyDinh, db.session))
-admin.add_view(MonHocView(MonHoc, db.session))
 admin.add_view(PhongHocView(PhongHoc, db.session))
+admin.add_view(MonHocView(MonHoc, db.session))
 admin.add_view(NhanVienView(NhanVien, db.session))
 admin.add_view(GiaoVienView(GiaoVien, db.session))
 admin.add_view(GiaoVienDayHocView(GiaoVienDayHoc, db.session))
